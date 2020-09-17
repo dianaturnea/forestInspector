315ab8fe893acfdab0a0685816b76901e213abc7
Diana Turnea <turnea.diana@gmail.com>
2020-09-17 22:37:17 +0300

    Add basic components

--- App.js
+++ App.js
@@ -1,13 +1,62 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  ScrollView,
+} from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Forest Inspector</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image
+            style={{ width: 200, height: 200, margin: 20 }}
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/31/bc/b4/31bcb437c29376a67742b4d5f502b721.png",
+            }}
+          />
+          <Image
+            style={{ width: 200, height: 200, margin: 20 }}
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/31/bc/b4/31bcb437c29376a67742b4d5f502b721.png",
+            }}
+          />
+          <Image
+            style={{ width: 200, height: 200, margin: 20 }}
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/31/bc/b4/31bcb437c29376a67742b4d5f502b721.png",
+            }}
+          />
+          <Image
+            style={{ width: 200, height: 200, margin: 20 }}
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/31/bc/b4/31bcb437c29376a67742b4d5f502b721.png",
+            }}
+          />
+          <Text>Forest Inspector</Text>
+          <TextInput
+            style={{
+              backgroundColor: "green",
+              width: 150,
+              paddingHorizontal: 15,
+              paddingVertical: 15,
+              margin: 15,
+            }}
+            placeholder={"Car input"}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
