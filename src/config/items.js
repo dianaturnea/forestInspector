a0afaba71519ea92074a1e598f236eee02e352ee
Diana Turnea <turnea.diana@gmail.com>
2020-09-18 08:12:11 +0300

    Add navigation

---
+++ src/config/items.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Car 01",
+    details: "Details 1",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Car 02",
+    details: "Details 2",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 03",
+    details: "Details 3",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 04",
+    details: "Details 4",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 05",
+    details: "Details 5",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 06",
+    details: "Details 6",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 07",
+    details: "Details 7",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 08",
+    details: "Details 8",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 09",
+    details: "Details 9",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 10",
+    details: "Details 10",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 11",
+    details: "Details 11",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Car 12",
+    details: "Details 12",
+  },
+];
